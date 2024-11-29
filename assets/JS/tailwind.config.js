60c1794a2a3589415db4d418e0af658019ac1f07
RamNikhil411 <nikhil.b@orotron.com>
2024-11-29 15:39:16 +0530

    Actonos home page

---
+++ assets/JS/tailwind.config.js
@@ -0,0 +1,29 @@
+tailwind.config={
+    theme:{
+        fontFamily:{
+            'myriad':['Myriad Pro'],
+            'bebasNeue':['Bebas Neue'],
+            'inter':['Inter'],
+            'dm_sans':['DM Sans'],
+            'dark_grotesque':['Darker Grotesque'],
+            'Satoshi':['Satoshi']
+        },
+        extend:{
+            animation:{
+                'loop-scroll':'loop-scroll 50s linear infinite',
+            },
+            keyframes:{
+                'loop-scroll':{
+                    from:{transform:'translateX(0)'},
+                    to:{transform:'translateX(-100%)'},
+                }
+            },
+            colors:{
+                'primary_color':'#EFEFEF',
+                'third_color':'#222222'
+            }
+
+            
+        }
+    }
+}

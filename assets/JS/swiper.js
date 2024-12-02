a34c26e432f3fe844ccd3a7979d9fe8e1b417ffa
RamNikhil411 <nikhil.b@orotron.com>
2024-12-02 12:36:11 +0530

    13 commit

--- assets/JS/swiper.js
+++ assets/JS/swiper.js
@@ -1,6 +1,12 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
+    loop:true,
+    autoplay:{
+        delay:3000,
+        disableOnInteraction:false
+
+    },
     navigation: {
         nextEl: ".next-button",
         prevEl: ".prev-button",
